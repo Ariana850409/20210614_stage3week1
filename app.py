0742da856ba9f0e6d9c277cb93044392ca92fded
puziyu <sarah850409@gmail.com>
2021-06-18 16:11:00 +0800

    add cdn path & RDS

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import *
 import boto3
+import pymysql
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -10,19 +11,53 @@ s3 = boto3.client(
     aws_secret_access_key=os.getenv('S3_Secret'),
 )
 
+# table=content -> id(int),picture(varchar),message(varchar)
+board_db = pymysql.connect(
+    host=os.getenv('RDS_Host'), user=os.getenv('RDS_User'), password=os.getenv('RDS_Password'), database="Message")
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
+@app.route("/download")
+def download():
+    try:
+        mycursor = board_db.cursor()
+        sql = "select (picture,message) from content"
+        mycursor.execute(sql)
+        myresult = mycursor.fetchall
+        result = []
+        for x in myresult:
+            y = {
+                "pic": x[0],
+                "text": x[1]
+            }
+            result.append(y)
+        board_db.close()
+        return jsonify({"data": result})
+    except:
+        return jsonify({"error": True})
+
+
 @app.route("/upload", methods=['POST'])
 def upload():
-    content = request.form.get('text')
-    picFile = request.files.get('pic')
-    s3.upload_fileobj(picFile, "first-time-test",
-                      picFile.filename, ExtraArgs={'ACL': 'public-read'})
-    return "success"
+    try:
+        content = request.form.get('text')
+        picFile = request.files.get('pic')
+        s3.upload_fileobj(picFile, "first-time-test",
+                          picFile.filename, ExtraArgs={'ACL': 'public-read'})
+        cdnPath = "http://d1ggvmbnmq1itc.cloudfront.net"+picFile.filename
+        mycursor = board_db.cursor()
+        sql = "insert into content(picture,message) values('%s','%s')" % (
+            cdnPath, content)
+        mycursor.execute(sql)
+        board_db.commit()
+        board_db.close()
+        return jsonify({"ok": True})
+    except:
+        return jsonify({"error": True})
 
 
 app.run(host="0.0.0.0", port=4000, debug=True)
--- static/index.js
+++ static/index.js
@@ -1,3 +1,37 @@
+function getData() {
+    fetch('/download', {
+        method: 'GET',
+    })
+        .then(res => {
+            return res.json();
+        }).then(result => {
+            console.log(result);
+            if (result.error) {
+                console.log("伺服器內部錯誤");
+            } else {
+                if (result.data.pic == null) {
+                    console.log("尚無任何留言");
+                } else if (result.data.pic != null) {
+                    for (let i = 0; i < result.data.length; i++) {
+                        let main = document.querySelector('main');
+                        let hr = document.createElement("hr");
+                        let cell = document.createElement("div");
+                        let text = document.createElement("div");
+                        let img = document.createElement("img");
+                        main.appendChild(hr);
+                        main.appendChild(cell);
+                        text.className = 'picText';
+                        text.textContent = result.data[i].text;
+                        cell.appendChild(text);
+                        img.classList.add("obj");
+                        img.src = result.data[i].pic;
+                        cell.appendChild(img);
+                    }
+                }
+            }
+        })
+}
+
 function handleFiles(pic) {
     let preview = document.querySelector(".preview");
     let previewPic = document.querySelector(".previewPic");
@@ -18,43 +52,29 @@ function submit() {
     let selectedPic = document.getElementById('uploadPic').files[0];
     console.log(selectedPic);
     let content = document.querySelector('.textInput').value;
-
-    let formData = new FormData();
-    formData.append("text", content);
-    formData.append("pic", selectedPic);
-    fetch('/upload', {
-        method: 'POST',
-        body: formData,
-    })
-    // .then(res => {
-    //     return res.json();
-    // }).then(result => {
-    //     console.log(result);
-    // })
-
-
     if (selectedPic != undefined && content != "") {
         document.querySelector('.errormsg').style.display = "none";
         let preview = document.querySelector(".preview");
         let previewPic = document.querySelector(".previewPic");
         preview.removeChild(previewPic);
-        let main = document.querySelector('main');
-        let hr = document.createElement("hr");
-        let cell = document.createElement("div");
-        let text = document.createElement("div");
-        let img = document.createElement("img");
-        main.appendChild(hr);
-        main.appendChild(cell);
-        text.className = 'picText';
-        text.textContent = content;
-        cell.appendChild(text);
-        img.classList.add("obj");
-        let objectURL = window.URL.createObjectURL(selectedPic);
-        console.log(objectURL);
-        img.src = objectURL;
-        cell.appendChild(img);
         document.querySelector('.textInput').value = "";
-
+        let formData = new FormData();
+        formData.append("text", content);
+        formData.append("pic", selectedPic);
+        fetch('/upload', {
+            method: 'POST',
+            body: formData,
+        })
+            .then(res => {
+                return res.json();
+            }).then(result => {
+                if (result.ok) {
+                    window.location.reload();
+                } else if (result.error) {
+                    document.querySelector('.errormsg').textContent = "留言上傳失敗";
+                    document.querySelector('.errormsg').style.display = "block";
+                }
+            })
     } else {
         document.querySelector('.errormsg').textContent = "請輸入文字或上傳照片";
         document.querySelector('.errormsg').style.display = "block";
